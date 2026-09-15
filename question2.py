83f9ad5a152dff19715f94f43b6353610a6d67be
jagtap-prajakta <prajaktajagtap5414@gmail.com>
2026-09-15 12:15:10 +0530

    sum of all numbers

--- question2.py
+++ question2.py
@@ -1,11 +1,17 @@
 # calculate the sum of all numbers from 1 to n 
 
-n = int(input("Enter a number: "))
+# n = int(input("Enter a number: "))
+# sum = 0
+# for i in range(1, n + 1):
+#     sum += i
+# print("The sum of all numbers from 1 to", n, "is:", sum)
+
+
+# calculate the sum of all numbers from 1 to n in a list 
+numbers = [1, 2, 3, 4, 5]
 sum = 0
-for i in range(1, n + 1):
-    sum += i
-print("The sum of all numbers from 1 to", n, "is:", sum)
+for num in numbers:
+    sum += num
+print("The sum of all numbers from 1 to n in the list is: ", sum)
 
-
-# calculate the sum of all numbers from 1 to n in a list accept list from user
 
