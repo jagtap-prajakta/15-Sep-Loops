2e6f177c5d01013a6b5535cc44e54885e36db974
jagtap-prajakta <prajaktajagtap5414@gmail.com>
2026-09-15 10:29:58 +0530

    second file

--- looping.py
+++ looping.py
@@ -17,14 +17,8 @@
 # print("Password Matched!")
 
 
-# # 3. 
-# correct_pass = "some_pass"
-# not_found = True
+# 3. above code in other way:
 
-# while pass == correct_pass:
-#     pass = input("Enter pass: ")
-#     not_found = False
-# print("Password Matched!")
 
 
 # # 4. 
@@ -34,8 +28,42 @@
 #     i += 1
 
 
-# 5. print table of 5:
-i = 1
-while i <= 10:
-    print(f"5 × {i} = {5*i}")
-    i += 1
+# # 5. print table of 5:
+# i = 1
+# while i <= 10:
+#     print(f"5 × {i} = {5*i}")
+#     i += 1
+
+
+# # 6. break statement 
+# correct_pass = "some_pass"
+# not_found = True
+
+# while not_found:
+#     passw = input("Enter pass: ")
+#     if passw == correct_pass:   #breaking condition
+#         break
+# print("Password Matched!")
+
+
+# # 7. continue statement : 
+# i = 0
+# while i < 10:
+#     if i == 5:
+#         continue
+#     print(i)
+#     i += 1    #output: 0 1 2 3 4 skip
+# # there's a bug in above program that will cause an infinite loop because when i == 5, the continue statement will skip the increment of i, so i will always be 5 and the loop will never terminate.
+# # difference between bug and error: bug is a logical error in the program which doesn't give the expected output but error is a syntax error which doesn't allow the program to run.
+
+# # to solve bug, we can increment i before the continue statement:
+# i = 0
+# while i < 10:
+#     if i == 5:
+#         i += 1
+#         continue
+#     print(i)
+#     i += 1    #output: 0 1 2 3 4 6 7 8 9
+
+
+
