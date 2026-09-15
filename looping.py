18aa3025353d1327d6a33df5d43c7519e3e7c1bd
jagtap-prajakta <prajaktajagtap5414@gmail.com>
2026-09-15 10:02:50 +0530

    table using while loop

--- looping.py
+++ looping.py
@@ -1,6 +1,41 @@
 # 15 Sep 2026
 # ---------------------------------------------- LOOPS  ----------------------------------------------
+# # 1.
+# while True:
+#     string = input("Enter a String: ")
+#     print(string)
 
-while True:
-    string = input("Enter a String: ")
-    print(string)
+
+# # 2. 
+# correct_pass = "some_pass"
+# not_found = True
+
+# while not_found:
+#     passw = input("Enter pass: ")
+#     if passw == correct_pass:   #breaking condition
+#         not_found = False
+# print("Password Matched!")
+
+
+# # 3. 
+# correct_pass = "some_pass"
+# not_found = True
+
+# while pass == correct_pass:
+#     pass = input("Enter pass: ")
+#     not_found = False
+# print("Password Matched!")
+
+
+# # 4. 
+# i = 0
+# while i < 10:
+#     print(i)
+#     i += 1
+
+
+# 5. print table of 5:
+i = 1
+while i <= 10:
+    print(f"5 × {i} = {5*i}")
+    i += 1
